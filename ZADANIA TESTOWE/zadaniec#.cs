94ba470d453bd1e1ad2c123824b38ae07d998b08
rudy123plpl <filipzielinskibb@gmail.com>
2024-10-22 13:21:23 +0200

    wykonano zadanie z c# ktore wymaga podania imienia a nastepnie je wyswietla

---
+++ ZADANIA TESTOWE/zadaniec#.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string)
+    {
+        Console.WriteLine("Jak masz na imię?");
+
+        string imie = Console.ReadLine();
+
+        Console.WriteLine("Twoje imię to: " + imie);
+    }
+}
