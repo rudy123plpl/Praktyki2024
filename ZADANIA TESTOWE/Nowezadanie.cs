64ee1633ab769a29c8f6df7afdcc6473357e0bf8
rudy123plpl <filipzielinskibb@gmail.com>
2024-10-22 22:18:35 +0200

    wykonano zadanie z c# ktore prosi o wypisanie 4 liczb calkowitych i nastepnie poprzez true/false sprawdza czy wszystkie wczesniej podane liczby sa parzyste czy nie

---
+++ ZADANIA TESTOWE/Nowezadanie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace siszarp
+{
+    class Xyz
+    {
+        static void Main(string[]args)
+        {
+            // Pobieranie liczb od użytkownika
+            Console.WriteLine("Podaj pierwszą liczbę:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj trzecią liczbę:");
+            int number3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj czwartą liczbę:");
+            int number4 = int.Parse(Console.ReadLine());
+
+            // Sprawdzanie, czy wszystkie liczby są parzyste
+            bool allEven = (number1 % 2 == 0) && (number2 % 2 == 0) && (number3 % 2 == 0) && (number4 % 2 == 0);
+
+            // Wypisanie wyniku
+            if (allEven)
+            {
+                Console.WriteLine("Czy wszystkie liczby są parzyste? Tak");
+            }
+            else
+            {
+                Console.WriteLine("Czy wszystkie liczby są parzyste? Nie");
+            }
+        }
+    }
+}
+
+
+
