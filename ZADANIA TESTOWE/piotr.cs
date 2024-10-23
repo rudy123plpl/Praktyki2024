c5cf7a70aac014dd9da4444d88fc7813f7bf18e5
rudy123plpl <filipzielinskibb@gmail.com>
2024-10-23 12:25:47 +0200

    Aplikacja konsoli C#, która pobiera od użytkownika liczbę całkowitą i sprawdza czy jest ona dodatnia, ujemna czy zerowa

---
+++ ZADANIA TESTOWE/piotr.cs
@@ -0,0 +1,36 @@
+﻿
+   using System;
+namespace siszarp
+{
+
+    class Piotr
+    {
+        static void Main()
+        {
+            Console.WriteLine("Podaj liczbę całkowitą:");
+            int liczba;
+
+            
+            if (int.TryParse(Console.ReadLine(), out liczba))
+            {
+                if (liczba > 0)
+                {
+                    Console.WriteLine("Liczba jest dodatnia.");
+                }
+                else if (liczba < 0)
+                {
+                    Console.WriteLine("Liczba jest ujemna.");
+                }
+                else
+                {
+                    Console.WriteLine("Liczba jest zerem.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowy format liczby. Proszę podać liczbę całkowitą.");
+            }
+        }
+    }
+}
+
